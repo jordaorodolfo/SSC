95e41b438b663741e53a4a6c4470ac262527a1d4
jordaorodolfo <jor.rodolfo@gmail.com>
2014-09-08 18:15:53 +0100

    mistakingly deleted sensor file

--- SSC_Arduino/Sensor.cpp
+++ SSC_Arduino/Sensor.cpp
@@ -25,20 +25,6 @@ Sensor_t * sensorInit(byte pin_, double min_value_, double max_value_,double min
   return new_sensor;
 }
 
-Sensor_t * sensorInitPullUp(byte pin_, double min_value_, double max_value_,double min_voltage_, double max_voltage_)
-{
-  Sensor_t * new_sensor = (Sensor_t*)malloc(sizeof(Sensor_t));
-  new_sensor->pin = pin_;
-  new_sensor->max_value = max_value_;
-  new_sensor->max_voltage = max_voltage_*1023.0/5.0;
-  new_sensor->min_value = min_value_;
-  new_sensor->min_voltage = min_voltage_*1023.0/5.0;
-  new_sensor->voltage_drift_linear = 0.0;
-  new_sensor->voltage_drift_const = 0.0;
-  pinMode(pin_,INPUT_PULLUP);
-  return new_sensor;
-}
-
 double sensorGetReading(Sensor_t * target)
 {
   return doubleMap(target->reading*(1+target->voltage_drift_linear)+target->voltage_drift_const,
