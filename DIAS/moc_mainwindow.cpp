7811ea94c32ebe30bd535288647c2b8f4c675ce6
jordaorodolfo <jor.rodolfo@gmail.com>
2015-01-16 01:01:23 +0000

    Added compiled version of Dias.
    Windows pre-compiled version of DIAS for 32 and 64 bits archs.

---
+++ DIAS/moc_mainwindow.cpp
@@ -0,0 +1,484 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.4.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../SSC-Interface/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#include <QtCore/QList>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.4.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[36];
+    char stringdata[726];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 11), // "sendArduino"
+QT_MOC_LITERAL(2, 23, 0), // ""
+QT_MOC_LITERAL(3, 24, 23), // "serialRecievedProcessed"
+QT_MOC_LITERAL(4, 48, 10), // "serialSent"
+QT_MOC_LITERAL(5, 59, 17), // "arduinoConnection"
+QT_MOC_LITERAL(6, 77, 11), // "motorEnable"
+QT_MOC_LITERAL(7, 89, 13), // "motorOutputCW"
+QT_MOC_LITERAL(8, 103, 16), // "motorReferenceCW"
+QT_MOC_LITERAL(9, 120, 13), // "motorStalling"
+QT_MOC_LITERAL(10, 134, 14), // "motorOutputRPM"
+QT_MOC_LITERAL(11, 149, 17), // "motorReferenceRPM"
+QT_MOC_LITERAL(12, 167, 25), // "motorReferenceReciproFreq"
+QT_MOC_LITERAL(13, 193, 22), // "motorOutputReciproFreq"
+QT_MOC_LITERAL(14, 216, 12), // "systemHeight"
+QT_MOC_LITERAL(15, 229, 13), // "statusMessage"
+QT_MOC_LITERAL(16, 243, 14), // "serialRecieved"
+QT_MOC_LITERAL(17, 258, 15), // "sendArduinoData"
+QT_MOC_LITERAL(18, 274, 25), // "on_inputRpm_returnPressed"
+QT_MOC_LITERAL(19, 300, 25), // "on_inputRpmButton_clicked"
+QT_MOC_LITERAL(20, 326, 28), // "on_motorEnableButton_clicked"
+QT_MOC_LITERAL(21, 355, 31), // "on_connectArduinoButton_clicked"
+QT_MOC_LITERAL(22, 387, 29), // "on_motorDisableButton_clicked"
+QT_MOC_LITERAL(23, 417, 29), // "on_actionOutput_Rpm_triggered"
+QT_MOC_LITERAL(24, 447, 37), // "on_inputReciproFreqpushButton..."
+QT_MOC_LITERAL(25, 485, 41), // "on_inputReciproFreqlineEdit_r..."
+QT_MOC_LITERAL(26, 527, 35), // "on_actionOutput_Frequency_tri..."
+QT_MOC_LITERAL(27, 563, 32), // "on_actionEditVariables_triggered"
+QT_MOC_LITERAL(28, 596, 7), // "setVars"
+QT_MOC_LITERAL(29, 604, 10), // "QList<int>"
+QT_MOC_LITERAL(30, 615, 9), // "int_vars_"
+QT_MOC_LITERAL(31, 625, 13), // "QList<double>"
+QT_MOC_LITERAL(32, 639, 12), // "double_vars_"
+QT_MOC_LITERAL(33, 652, 13), // "processHeight"
+QT_MOC_LITERAL(34, 666, 33), // "on_actionDefault_Values_trigg..."
+QT_MOC_LITERAL(35, 700, 25) // "on_actionHeight_triggered"
+
+    },
+    "MainWindow\0sendArduino\0\0serialRecievedProcessed\0"
+    "serialSent\0arduinoConnection\0motorEnable\0"
+    "motorOutputCW\0motorReferenceCW\0"
+    "motorStalling\0motorOutputRPM\0"
+    "motorReferenceRPM\0motorReferenceReciproFreq\0"
+    "motorOutputReciproFreq\0systemHeight\0"
+    "statusMessage\0serialRecieved\0"
+    "sendArduinoData\0on_inputRpm_returnPressed\0"
+    "on_inputRpmButton_clicked\0"
+    "on_motorEnableButton_clicked\0"
+    "on_connectArduinoButton_clicked\0"
+    "on_motorDisableButton_clicked\0"
+    "on_actionOutput_Rpm_triggered\0"
+    "on_inputReciproFreqpushButton_clicked\0"
+    "on_inputReciproFreqlineEdit_returnPressed\0"
+    "on_actionOutput_Frequency_triggered\0"
+    "on_actionEditVariables_triggered\0"
+    "setVars\0QList<int>\0int_vars_\0QList<double>\0"
+    "double_vars_\0processHeight\0"
+    "on_actionDefault_Values_triggered\0"
+    "on_actionHeight_triggered"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      33,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+      16,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,  179,    2, 0x06 /* Public */,
+       1,    0,  182,    2, 0x06 /* Public */,
+       3,    0,  183,    2, 0x06 /* Public */,
+       4,    0,  184,    2, 0x06 /* Public */,
+       5,    1,  185,    2, 0x06 /* Public */,
+       6,    1,  188,    2, 0x06 /* Public */,
+       7,    1,  191,    2, 0x06 /* Public */,
+       8,    1,  194,    2, 0x06 /* Public */,
+       9,    1,  197,    2, 0x06 /* Public */,
+      10,    1,  200,    2, 0x06 /* Public */,
+      11,    1,  203,    2, 0x06 /* Public */,
+      12,    1,  206,    2, 0x06 /* Public */,
+      13,    1,  209,    2, 0x06 /* Public */,
+      14,    1,  212,    2, 0x06 /* Public */,
+      15,    2,  215,    2, 0x06 /* Public */,
+      15,    1,  220,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+      16,    0,  223,    2, 0x0a /* Public */,
+      17,    1,  224,    2, 0x0a /* Public */,
+      17,    0,  227,    2, 0x0a /* Public */,
+      18,    0,  228,    2, 0x08 /* Private */,
+      19,    0,  229,    2, 0x08 /* Private */,
+      20,    0,  230,    2, 0x08 /* Private */,
+      21,    0,  231,    2, 0x08 /* Private */,
+      22,    0,  232,    2, 0x08 /* Private */,
+      23,    0,  233,    2, 0x08 /* Private */,
+      24,    0,  234,    2, 0x08 /* Private */,
+      25,    0,  235,    2, 0x08 /* Private */,
+      26,    0,  236,    2, 0x08 /* Private */,
+      27,    0,  237,    2, 0x08 /* Private */,
+      28,    2,  238,    2, 0x08 /* Private */,
+      33,    1,  243,    2, 0x08 /* Private */,
+      34,    0,  246,    2, 0x08 /* Private */,
+      35,    0,  247,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Bool,    2,
+    QMetaType::Void, QMetaType::Bool,    2,
+    QMetaType::Void, QMetaType::Bool,    2,
+    QMetaType::Void, QMetaType::Bool,    2,
+    QMetaType::Void, QMetaType::Bool,    2,
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void, QMetaType::QString, QMetaType::Int,    2,    2,
+    QMetaType::Void, QMetaType::QString,    2,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 29, 0x80000000 | 31,   30,   32,
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        switch (_id) {
+        case 0: _t->sendArduino((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 1: _t->sendArduino(); break;
+        case 2: _t->serialRecievedProcessed(); break;
+        case 3: _t->serialSent(); break;
+        case 4: _t->arduinoConnection((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 5: _t->motorEnable((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 6: _t->motorOutputCW((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 7: _t->motorReferenceCW((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 8: _t->motorStalling((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 9: _t->motorOutputRPM((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 10: _t->motorReferenceRPM((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 11: _t->motorReferenceReciproFreq((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 12: _t->motorOutputReciproFreq((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 13: _t->systemHeight((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 14: _t->statusMessage((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 15: _t->statusMessage((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 16: _t->serialRecieved(); break;
+        case 17: _t->sendArduinoData((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 18: _t->sendArduinoData(); break;
+        case 19: _t->on_inputRpm_returnPressed(); break;
+        case 20: _t->on_inputRpmButton_clicked(); break;
+        case 21: _t->on_motorEnableButton_clicked(); break;
+        case 22: _t->on_connectArduinoButton_clicked(); break;
+        case 23: _t->on_motorDisableButton_clicked(); break;
+        case 24: _t->on_actionOutput_Rpm_triggered(); break;
+        case 25: _t->on_inputReciproFreqpushButton_clicked(); break;
+        case 26: _t->on_inputReciproFreqlineEdit_returnPressed(); break;
+        case 27: _t->on_actionOutput_Frequency_triggered(); break;
+        case 28: _t->on_actionEditVariables_triggered(); break;
+        case 29: _t->setVars((*reinterpret_cast< QList<int>(*)>(_a[1])),(*reinterpret_cast< QList<double>(*)>(_a[2]))); break;
+        case 30: _t->processHeight((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 31: _t->on_actionDefault_Values_triggered(); break;
+        case 32: _t->on_actionHeight_triggered(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 29:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 1:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QList<double> >(); break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QList<int> >(); break;
+            }
+            break;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        void **func = reinterpret_cast<void **>(_a[1]);
+        {
+            typedef void (MainWindow::*_t)(QString );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MainWindow::sendArduino)) {
+                *result = 0;
+            }
+        }
+        {
+            typedef void (MainWindow::*_t)();
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MainWindow::sendArduino)) {
+                *result = 1;
+            }
+        }
+        {
+            typedef void (MainWindow::*_t)();
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MainWindow::serialRecievedProcessed)) {
+                *result = 2;
+            }
+        }
+        {
+            typedef void (MainWindow::*_t)();
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MainWindow::serialSent)) {
+                *result = 3;
+            }
+        }
+        {
+            typedef void (MainWindow::*_t)(bool );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MainWindow::arduinoConnection)) {
+                *result = 4;
+            }
+        }
+        {
+            typedef void (MainWindow::*_t)(bool );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MainWindow::motorEnable)) {
+                *result = 5;
+            }
+        }
+        {
+            typedef void (MainWindow::*_t)(bool );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MainWindow::motorOutputCW)) {
+                *result = 6;
+            }
+        }
+        {
+            typedef void (MainWindow::*_t)(bool );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MainWindow::motorReferenceCW)) {
+                *result = 7;
+            }
+        }
+        {
+            typedef void (MainWindow::*_t)(bool );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MainWindow::motorStalling)) {
+                *result = 8;
+            }
+        }
+        {
+            typedef void (MainWindow::*_t)(QString );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MainWindow::motorOutputRPM)) {
+                *result = 9;
+            }
+        }
+        {
+            typedef void (MainWindow::*_t)(QString );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MainWindow::motorReferenceRPM)) {
+                *result = 10;
+            }
+        }
+        {
+            typedef void (MainWindow::*_t)(QString );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MainWindow::motorReferenceReciproFreq)) {
+                *result = 11;
+            }
+        }
+        {
+            typedef void (MainWindow::*_t)(QString );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MainWindow::motorOutputReciproFreq)) {
+                *result = 12;
+            }
+        }
+        {
+            typedef void (MainWindow::*_t)(QString );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MainWindow::systemHeight)) {
+                *result = 13;
+            }
+        }
+        {
+            typedef void (MainWindow::*_t)(QString , int );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MainWindow::statusMessage)) {
+                *result = 14;
+            }
+        }
+        {
+            typedef void (MainWindow::*_t)(QString );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MainWindow::statusMessage)) {
+                *result = 15;
+            }
+        }
+    }
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata))
+        return static_cast<void*>(const_cast< MainWindow*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 33)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 33;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 33)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 33;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void MainWindow::sendArduino(QString _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+
+// SIGNAL 1
+void MainWindow::sendArduino()
+{
+    QMetaObject::activate(this, &staticMetaObject, 1, Q_NULLPTR);
+}
+
+// SIGNAL 2
+void MainWindow::serialRecievedProcessed()
+{
+    QMetaObject::activate(this, &staticMetaObject, 2, Q_NULLPTR);
+}
+
+// SIGNAL 3
+void MainWindow::serialSent()
+{
+    QMetaObject::activate(this, &staticMetaObject, 3, Q_NULLPTR);
+}
+
+// SIGNAL 4
+void MainWindow::arduinoConnection(bool _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 4, _a);
+}
+
+// SIGNAL 5
+void MainWindow::motorEnable(bool _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 5, _a);
+}
+
+// SIGNAL 6
+void MainWindow::motorOutputCW(bool _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 6, _a);
+}
+
+// SIGNAL 7
+void MainWindow::motorReferenceCW(bool _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 7, _a);
+}
+
+// SIGNAL 8
+void MainWindow::motorStalling(bool _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 8, _a);
+}
+
+// SIGNAL 9
+void MainWindow::motorOutputRPM(QString _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 9, _a);
+}
+
+// SIGNAL 10
+void MainWindow::motorReferenceRPM(QString _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 10, _a);
+}
+
+// SIGNAL 11
+void MainWindow::motorReferenceReciproFreq(QString _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 11, _a);
+}
+
+// SIGNAL 12
+void MainWindow::motorOutputReciproFreq(QString _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 12, _a);
+}
+
+// SIGNAL 13
+void MainWindow::systemHeight(QString _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 13, _a);
+}
+
+// SIGNAL 14
+void MainWindow::statusMessage(QString _t1, int _t2)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)), const_cast<void*>(reinterpret_cast<const void*>(&_t2)) };
+    QMetaObject::activate(this, &staticMetaObject, 14, _a);
+}
+
+// SIGNAL 15
+void MainWindow::statusMessage(QString _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 15, _a);
+}
+QT_END_MOC_NAMESPACE
--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Complete Repo
 The SSC-Arduino contains the files necessary for the arduino bridge in this project.
 The SSC-Interface contains all the Qt interface files and project Directives
 The SSC-Math contains all the files used in the modelling, such as the maple, math setc.
-
+The DIAS Folder contains a standalone compiled version of the SSC-Interface software for windows 32 and 64 bits.
 
 Rodolfo Jordao, 2014.
